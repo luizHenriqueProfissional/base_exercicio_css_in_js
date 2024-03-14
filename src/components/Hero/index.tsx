14257713ec08111129ead44c596cf147438b8883
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-14 00:42:16 -0300

    Componetizações realizadas

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,12 @@
-import styles from './Hero.module.css'
+import { Container } from '../../styles'
+import { Form } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <Form>
+    <Container>
+      As melhores vagas para tecnologia, design e artes visuais.
+    </Container>
+  </Form>
 )
 
 export default Hero
