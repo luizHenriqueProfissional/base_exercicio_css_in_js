6ca1bc870991100ebc149ad079639522390c1878
luizHenriqueProfissional <166259904+luizHenriqueProfissional@users.noreply.github.com>
2025-01-22 06:12:29 -0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,1 +1,14 @@
-import { Hero } from './HeroStyled'
+import { Container } from '../../styles'
+import { Form, H2Title } from './styles'
+
+const Hero = () => (
+  <Form>
+    <Container>
+      <H2Title>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </H2Title>
+    </Container>
+  </Form>
+)
+
+export default Hero
